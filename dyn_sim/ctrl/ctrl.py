c0acbec6323ebbf1769a94f14350ffd6072452f6
alberthli <alberthli@berkeley.edu>
2022-05-14 19:30:25 -0700

    initialized abstract controller class

---
+++ dyn_sim/ctrl/ctrl.py
@@ -0,0 +1,42 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+from dyn_sim.sys.dyn_sys import System
+
+
+class Controller(ABC):
+    """Abstract class for controllers."""
+
+    def __init__(self, sys: System) -> None:
+        """Initialize a controller.
+
+        Parameters
+        ----------
+        sys : System
+                Dynamical system to be controlled.
+        """
+        super(Controller, self).__init__()
+
+        self._n = sys._n
+        self._m = sys._m
+
+    @abstractmethod
+    def ctrl(self, t: float, x: np.ndarray) -> np.ndarray:
+        """Control law.
+
+        Parameters
+        ----------
+        t: float
+            Time.
+        x: np.ndarray, shape=(n,)
+            State.
+
+        Returns
+        -------
+        u: np.ndarray, shape=(m,)
+            Control input.
+        """
+
+    def reset(self) -> None:
+        """Resets controller internals. Used for controllers with memory."""
