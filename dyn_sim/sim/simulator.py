36214f49e0aa01f93cc03a169048dfe425245424
alberthli <alberthli@berkeley.edu>
2022-05-15 14:20:55 -0700

    basic simulator (wrapping solve_ivp)

---
+++ dyn_sim/sim/simulator.py
@@ -0,0 +1,183 @@
+from typing import Callable
+
+import numpy as np
+from scipy.integrate import solve_ivp
+
+from dyn_sim.ctrl.ctrl import Controller
+from dyn_sim.sys.dyn_sys import System
+
+
+class SimulationEnvironment:
+    """Simulation environment for the system."""
+
+    def __init__(self, sys: System, ctrler: Controller) -> None:
+        """Initialize the simulator.
+
+        Parameters
+        ----------
+        sys : System
+            A dynamical system.
+        ctrler: Controller
+            A controller object.
+        """
+        self._sys = sys
+        self._ctrler = ctrler
+
+        self._fig = None
+        self._ax = None
+
+        # [TODO] port this functionality into viz functions
+        # self._fig = plt.figure()
+        # self._ax = p3.Axes3D(self._fig)
+        # self._ax.set_proj_type("ortho")
+        # self._ax.grid(False)
+        # self._ax.set_xticks([])
+        # self._ax.set_yticks([])
+        # self._ax.set_zticks([])
+        # self._ax.set_xlim3d(xlim)
+        # self._ax.set_ylim3d(ylim)
+        # self._ax.set_zlim3d(zlim)
+
+    def _clear_frame(self) -> None:
+        """Clear the environment frame."""
+        if self._ax is not None:
+            for artist in self._ax.lines + self._ax.collections:
+                artist.remove()
+
+    def simulate(
+        self,
+        x0: np.ndarray,
+        tsim: np.ndarray,
+        dfunc: Callable[[float, np.ndarray], np.ndarray] = None,
+        max_step: float = 0.01,
+    ) -> np.ndarray:
+        """Simulate a quadrotor run.
+
+        Parameters
+        ----------
+        x0 : np.ndarray, shape=(n,)
+            Initial state.
+        tsim : np.ndarray, shape=(T,)
+            Simulation query points.
+        dfunc : Callable[np.ndarray, np.ndarray]
+            Disturbance function. Takes in state and time and returns a
+            simulated disturbance.
+        max_step : float, default=0.01
+            Maximum time step in ODE solver.
+
+        Returns
+        -------
+        t_sol : np.ndarray, shape=(T,)
+            Time values associated with state solution.
+        x_sol : np.ndarray, shape=(n, T)
+            Solution trajectories at the query times.
+        """
+        assert x0.shape == (self._sys._n,)
+        assert tsim.ndim == 1
+
+        sys = self._sys
+
+        # controller
+        ctrl = lambda t, x: self._ctrler.ctrl(t, x)
+
+        # disturbance function
+        if dfunc is not None:
+            dyn = lambda t, x: sys.dyn(t, x, ctrl(t, x), dfunc(t, x))
+        else:
+            dyn = lambda t, x: sys.dyn(t, x, ctrl(t, x))
+
+        # simulating dynamics
+        sol = solve_ivp(dyn, (tsim[0], tsim[-1]), x0, t_eval=tsim, max_step=max_step)
+        t_sol = sol.t
+        x_sol = sol.y
+
+        self._ctrler.reset()
+
+        return t_sol, x_sol
+
+    # def simulate(
+    #     self,
+    #     s0: np.ndarray,
+    #     tsim: np.ndarray,
+    #     dfunc: Callable[[float, np.ndarray], np.ndarray] = None,
+    #     animate: bool = False,
+    #     anim_name: str = None,
+    #     fps: float = 10.0,
+    # ) -> np.ndarray:
+    #     """Simulate a quadrotor run.
+
+    #     Parameters
+    #     ----------
+    #     s0: np.ndarray, shape=(12,)
+    #         Initial state.
+    #     tsim: np.ndarray, shape=(T,)
+    #         Simulation query points.
+    #     dfunc: Callable[np.ndarray, np.ndarray]
+    #         Disturbance function. Takes in state and time and returns a
+    #         simulated disturbance.
+    #     animate: bool
+    #         Flag for whether an animation of the run should play.
+    #     anim_name: str
+    #         Name for animation file.
+    #     fps: float
+    #         Animation frames per second.
+
+    #     Returns
+    #     -------
+    #     s_sol: np.ndarray, shape=(12, T)
+    #         Solution trajectories at the query times.
+    #     """
+    #     assert s0.shape == (12,)
+    #     assert tsim.ndim == 1
+
+    #     quad = self._quad
+
+    #     # controller
+    #     if type(self._ctrler) == MultirateQuadController:
+    #         ctrl = lambda t, s: self._ctrler.ctrl(t, s, self._obs_list)
+    #     else:
+    #         ctrl = lambda t, s: self._ctrler.ctrl(t, s)
+
+    #     # disturbance function
+    #     if dfunc is not None:
+    #         dyn = lambda t, s: quad._dyn(s, ctrl(t, s), dfunc(t, s))
+    #     else:
+    #         dyn = lambda t, s: quad._dyn(s, ctrl(t, s))
+
+    #     # simulating dynamics
+    #     sol = solve_ivp(
+    #         dyn, (tsim[0], tsim[-1]), s0, t_eval=tsim, max_step=self._ctrler._sim_dt
+    #     )  # cap framerate of reality
+    #     s_sol = sol.y
+
+    #     # Get ref traj for plotting
+    #     ctrler = self._ctrler
+    #     ref = ctrler._ref
+    #     ref_traj = np.zeros((12, s_sol.shape[1]))
+    #     for i in range(s_sol.shape[1]):
+    #         ref_traj[:, i] = ref(tsim[i])
+
+    #     self._ctrler.reset()
+
+    #     # animation
+    #     if animate:
+    #         self._draw_obs()
+
+    #         def _anim_quad(i):
+    #             self._clear_frame()
+    #             self._draw_quad(s_sol[:, i])
+    #             self._draw_traj(s_sol, ref_traj, i)
+
+    #         anim = animation.FuncAnimation(
+    #             self._fig, _anim_quad, interval=20.0, frames=len(tsim)
+    #         )
+
+    #         if anim_name is not None:
+    #             Writer = animation.writers["ffmpeg"]
+    #             writer = Writer(fps=fps, bitrate=1800)
+    #             anim.save("{}.mp4".format(anim_name), writer=writer)
+
+    #         plt.show()
+    #         self._clear_frame(clear_obs=True)
+
+    #     return s_sol
--- scripts/ex_pd_quad_ctrl.py
+++ scripts/ex_pd_quad_ctrl.py
@@ -2,11 +2,11 @@ import sys
 
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import solve_ivp
 
 sys.path.append("..")
 
 from dyn_sim.ctrl.quad.pd_quad_ctrl import PDQuadController  # noqa: E402
+from dyn_sim.sim.simulator import SimulationEnvironment  # noqa: E402
 from dyn_sim.sys.spatial.quad import Quadrotor  # noqa: E402
 
 # QUADROTOR #
@@ -19,60 +19,54 @@ Jtp = 0.1  # Optional: total rot moment about prop axes (gyro)
 
 quad = Quadrotor(m, I, kf, km, l, Jtp)
 
-
-# CONTROLLERS #
-
-# PD controller
+# PD CONTROLLER #
 kp_xyz = 0.01  # gains for Cartesian position control
 kd_xyz = 0.04
 kp_a = 10  # gains for attitude control
 kd_a = 5
-ref = lambda t: np.array([np.cos(t), np.sin(t), 0, 0])  # reference
+ref = lambda t: np.array(
+    [np.cos(t), np.sin(t), 0, 0]
+)  # (x, y, z, yaw), pitch/roll assumed 0
 
 pdc = PDQuadController(quad, kp_xyz, kd_xyz, kp_a, kd_a, ref)
 
+# SIMULATION ENVIRONMENT #
+simulator = SimulationEnvironment(quad, pdc)
 
 if __name__ == "__main__":
     # running simulation
-    s0 = np.zeros(12)  # initial state
+    x0 = np.zeros(12)  # initial state
     sim_length = 30.0  # simulation time
-    frames = int(10 * sim_length + 1)  # number of frames
-    fps = 20.0  # animation fps
-    tsim = np.linspace(0, sim_length, frames)  # query times
-
-    ctrl = lambda t, s: pdc.ctrl(t, s)
-    dyn = lambda t, s: quad.dyn(t, s, ctrl(t, s))
-
-    sol = solve_ivp(
-        dyn, (tsim[0], tsim[-1]), s0, t_eval=tsim, max_step=0.01
-    )  # cap framerate of reality
-    sim_data = sol.y
+    n_frames = int(10 * sim_length + 1)  # number of frames
+    # fps = 20.0  # animation fps
+    tsim = np.linspace(0, sim_length, n_frames)  # query times
+    t_sol, x_sol = simulator.simulate(x0, tsim)
 
     # plotting positional results
-    o = sim_data[0:3, :]
-    alpha = sim_data[3:6, :]
+    o = x_sol[0:3, :]
+    alpha = x_sol[3:6, :]
 
     fig, axs = plt.subplots(2, 1)
     fig.tight_layout(pad=3.0)
 
     # positions
-    axs[0].plot(tsim, o[0, :].T, color="orange")
-    axs[0].plot(tsim, o[1, :].T, color="green")
-    axs[0].plot(tsim, o[2, :].T, color="blue")
+    axs[0].plot(t_sol, o[0, :].T, color="orange")
+    axs[0].plot(t_sol, o[1, :].T, color="green")
+    axs[0].plot(t_sol, o[2, :].T, color="blue")
     axs[0].legend(["x", "y", "z"], loc="lower left", ncol=3)
-    axs[0].set_title("Quadrotor Position and Reference Trajectory")
+    axs[0].set_title("Quadrotor Position")
     axs[0].set_xlabel("time")
     axs[0].set_ylabel("position")
     axs[0].set_ylim([-2, 2])
-    axs[0].set_xlim([tsim[0], tsim[-1]])
+    axs[0].set_xlim([t_sol[0], t_sol[-1]])
 
     # angles
-    axs[1].plot(tsim, alpha[0, :].T, color="orange")
-    axs[1].plot(tsim, alpha[1, :].T, color="green")
+    axs[1].plot(t_sol, alpha[0, :].T, color="orange")
+    axs[1].plot(t_sol, alpha[1, :].T, color="green")
     axs[1].legend(["roll", "pitch"], loc="lower left", ncol=2)
-    axs[1].set_title("Quadrotor Roll/Pitch and Safety Limits")
+    axs[1].set_title("Quadrotor Roll and Pitch")
     axs[1].set_xlabel("time")
     axs[1].set_ylabel("angle")
-    axs[1].set_xlim([tsim[0], tsim[-1]])
+    axs[1].set_xlim([t_sol[0], t_sol[-1]])
 
     plt.show()  # TODO: fix this also showing the simulator when unwanted
