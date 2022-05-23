d68edb26d17080d3993ceb370628ed44e944b010
alberthli <alberthli@berkeley.edu>
2022-05-23 14:59:15 -0700

    added impl for BWLC ctrl method

--- dyn_sim/ctrl/ctrl.py
+++ dyn_sim/ctrl/ctrl.py
@@ -56,6 +56,61 @@ class BWLC(Controller):
         wc : float
             The control frequency (Hz).
         """
-        assert wc > 0
+        assert wc > 0.0
+
         super(BWLC, self).__init__()
         self._wc = wc
+        self._dt = 1.0 / wc  # convenience dt for given wc
+        self._tmem = None  # memory for last ctrl update time
+        self._u = None  # memory for current ZOH ctrl input
+
+    @abstractmethod
+    def _ctrl_update(self, t: float, x: np.ndarray) -> np.ndarray:
+        """Update the control input.
+
+        This function is called internally by the ctrl(...) method.
+
+        Parameters
+        ----------
+        t: float
+            Time.
+        x: np.ndarray, shape=(n,)
+            State.
+
+        Returns
+        -------
+        u: np.ndarray, shape=(m,)
+            Control input.
+        """
+
+    def ctrl(self, t: float, x: np.ndarray) -> np.ndarray:
+        """Bandwidth-limited control law.
+
+        This function ensures that the control updates are only applied at the rate of the controller bandwidth, zero-order holding them otherwise.
+
+        Parameters
+        ----------
+        See parent function.
+
+        Returns
+        -------
+        See parent function.
+        """
+        assert x.shape == (self._n,)
+
+        # memory initialization
+        if self._tmem is None:
+            self._tmem = t
+            self._u = self._ctrl_update(t, x)
+
+        # control update if enough time has elapsed
+        elif (t - self._tmem) > self._dt:
+            self._tmem = self._tmem + self._dt
+            self._u = self._ctrl_update(t, x)
+
+        return self._u
+
+    def reset(self) -> None:
+        """Resets the controller memory."""
+        self._tmem = None
+        self._u = None
